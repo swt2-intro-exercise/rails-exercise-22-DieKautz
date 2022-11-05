fba24e7d1eec9c4fb19e9811c9469da8b3e304c7
Malte <maltestellmacher@gmail.com>
2022-11-05 16:41:21 +0100

    Do not let paper validate without venue, fixes #17

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,3 @@
 class Paper < ApplicationRecord
-    validates :title, presence: true
-    validates :year, presence: true
+    validates :venue, presence: true
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should not validate without vanue" do
+    paper = Paper.new
+    paper.title="TEST"
+    expect(paper).to_not be_valid
+  end
 end
