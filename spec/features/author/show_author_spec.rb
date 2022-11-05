570f100d6d5838eb8c92a29422b0a257e2aae247
Malte <maltestellmacher@gmail.com>
2022-11-05 15:13:20 +0100

    Spec for show author link to edit page #12

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -10,4 +10,12 @@ describe "Show author page", type: :feature do
     expect(page).to have_text('http://wikipedia.de/alan_turing')
   end
 
+  it "should link to author's edit page" do
+    @alan = FactoryBot.create :author
+    visit author_path(@alan)
+
+    expect(page).to have_link(edit_author_path(@alan))
+  end
+
+
 end
