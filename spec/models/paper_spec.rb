4f08107a950e1797c77db813085c2343efa14da5
Malte <maltestellmacher@gmail.com>
2022-11-05 17:02:42 +0100

    Adjust spec for paper validation

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -3,9 +3,24 @@ require 'rails_helper'
 RSpec.describe Paper, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
-  it "should not validate without vanue" do
+  it "should not validate without venue" do
     paper = Paper.new
     paper.title="TEST"
+    paper.year=2022
+    expect(paper).to_not be_valid
+  end
+
+  it "should not validate without year" do
+    paper = Paper.new
+    paper.title="TEST"
+    paper.venue="Germany"
+    expect(paper).to_not be_valid
+  end
+
+  it "should not validate without title" do
+    paper = Paper.new
+    paper.venue="Germany"
+    paper.year=2022
     expect(paper).to_not be_valid
   end
 end
