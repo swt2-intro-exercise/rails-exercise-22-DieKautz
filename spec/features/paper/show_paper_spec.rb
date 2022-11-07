19270cbe6bb3ed8c91b6691e276b7cd08ae37e33
Malte <maltestellmacher@gmail.com>
2022-11-07 14:01:45 +0100

    Expect paper show page to list author names

--- spec/features/paper/show_paper_spec.rb
+++ spec/features/paper/show_paper_spec.rb
@@ -24,5 +24,12 @@ describe "Show Paper page", type: :feature do
     expect(page).to have_css("a[data-turbo-method='delete'][href='#{paper_path(@comp)}']")
   end
 
+  it "should display all authors full name" do
+    @comp = FactoryBot.create :paper
+    visit paper_path(@comp)
+
+    expect(page).to have_text(@comp.authors[0].name)
+  end
+
 
 end
