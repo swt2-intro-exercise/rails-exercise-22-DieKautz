2157e0806b668db917de1e5b26f371decc5a8165
Malte <maltestellmacher@gmail.com>
2022-11-07 13:57:56 +0100

    Add author paper relation

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -31,4 +31,9 @@ RSpec.describe Paper, type: :model do
     paper.year="abc"
     expect(paper).to_not be_valid
   end
+
+  it "should have an empty list of authors" do
+    paper = Paper.new
+    expect(paper.authors).to be_empty
+  end
 end
