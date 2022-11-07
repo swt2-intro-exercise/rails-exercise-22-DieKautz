42acc40eb9beb9b49a15e698bb4e4a79cb513d31
Malte <maltestellmacher@gmail.com>
2022-11-07 14:30:36 +0100

    Expect papers index to allow filtering by year

--- spec/features/paper/index_paper_spec.rb
+++ spec/features/paper/index_paper_spec.rb
@@ -9,4 +9,18 @@ describe "Index paper page", type: :feature do
     expect(find('tr')).to have_text('Year')
     expect(page).to have_link 'New', href: new_paper_path
   end
+
+  it "should filter papers by year" do
+    @p1 = FactoryBot.create :paper
+    p1.title = "50er paper"
+    p1.year = 1950
+    @p2 = FactoryBot.create :paper
+    p2.title = "68er paper"
+    p2.year = 1968
+
+    visit papers_path, year: 1950
+
+    expect(page).to have_text(@p1.title)
+    expect(page).to_not have_text(@p2.title)
+  end
 end
